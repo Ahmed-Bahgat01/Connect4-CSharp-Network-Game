f952e4161547a36100bfb176d6aad0d93b413788
Mohamedkh30 <mohamed_khaled_1999@yahoo.com>
2023-03-03 16:42:18 +0200

    Revert "a commit to pull"
    This reverts commit 95f82ef5a48ccded74c8402d0f7fcde999f1af4b.

--- ServerSide/ClientSide/SignForm.cs
+++ ServerSide/ClientSide/SignForm.cs
@@ -193,11 +193,15 @@ namespace ClientSide
         {
             if (StartConnection())
                 SignIn();
+            HomePage home = new HomePage();
+            home.Show();
         }
         private void SignUpBtn_Click(object sender, EventArgs e)
         {
             if(StartConnection())
                 SignUp();
+            HomePage home = new HomePage();
+            home.Show();
         }
     }
 }
